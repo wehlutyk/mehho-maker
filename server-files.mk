e34270c9e8769c0622cb089bdfd3d71a1df18540
Sébastien Lerique <seblerique@wanadoo.fr>
2013-02-12 00:16:54 +0100

    New tries at bugfixes

--- server-files.mk
+++ server-files.mk
@@ -5,7 +5,7 @@ server-files-dir.i :
 
 server-files-sharedfolders-perms.i : server-files-dir.i users.i
 	cp $(files_dir)/usr/local/bin/sharedfolders-perms /usr/local/bin/sharedfolders-perms
-	chmod 750 /usr/local/bin/sharedfolders-perms
+	chmod 755 /usr/local/bin/sharedfolders-perms
 	touch $@
 
 server-files-cron.i : server-files-dir.i server-files-sharedfolders-perms.i
