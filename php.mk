df1bddd120f2dd32c51e7a92e94341ebe7d91035
Sébastien Lerique <seblerique@wanadoo.fr>
2013-02-12 13:10:33 +0100

    Fix sed command

--- php.mk
+++ php.mk
@@ -4,7 +4,7 @@ php-pkg.i :
 
 php5-fpm-config.i : php-pkg.i utils.i
 	$(jinja_copy) $(files_dir)/etc/php5/fpm/php.ini.jinja $(settings) /etc/php5/fpm/php.ini
-	sed -i /etc/php5/fpm/pool.d/www.conf 's/^;security\.limit_extensions =.*$/security.limit_extensions = .php .php3 .php4 .php5 .css .js/'
+	sed -i 's/^;security\.limit_extensions =.*$/security.limit_extensions = .php .php3 .php4 .php5 .css .js/' /etc/php5/fpm/pool.d/www.conf
 	touch $@
 
 php5-fpm-service.i : php-pkg.i php5-fpm-config.i setup-php5-sqlite2.i php5-sqlite2-config.i
