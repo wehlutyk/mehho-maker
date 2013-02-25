d178fa454e3a25bb4378138dc10a38723194e99b
Sébastien Lerique <seblerique@wanadoo.fr>
2013-02-25 18:42:12 +0100

    Fix prosody configuration

--- prosody.mk
+++ prosody.mk
@@ -3,11 +3,16 @@ prosody-pkg.i : sqlite.i
 	touch $@
 
 prosody-config.i : prosody-pkg.i
+	# Prosody settings
 	$(jinja_copy) $(files_dir)/etc/prosody/prosody.cfg.lua.jinja $(settings) /etc/prosody/prosody.cfg.lua
 	chmod 640 /etc/prosody/prosody.cfg.lua
+	# Spool folder for dovecot auth
 	mkdir -p /var/spool/prosody/private
 	chmod 700 /var/spool/prosody/private
 	chown prosody /var/spool/prosody/private
+	# Dovecot auth module
+	ls /var/lib/prosody/modules/mod_auth_dovecot.lua || cd /var/lib/prosody/modules && wget http://prosody-modules.googlecode.com/hg/mod_auth_dovecot/auth_dovecot/mod_auth_dovecot.lua
+	ls /var/lib/prosody/modules/sasl_dovecot.lib.lua || cd /var/lib/prosody/modules && wget http://prosody-modules.googlecode.com/hg/mod_auth_dovecot/auth_dovecot/sasl_dovecot.lib.lua
 	touch $@
 
 ufw-allow-xmpp-c2s.i : ufw-pkg.i
