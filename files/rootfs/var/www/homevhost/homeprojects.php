1eb4593da8d4c48d4f68e050e5929d6b2b88c316
Sébastien Lerique <seblerique@wanadoo.fr>
2013-07-04 23:06:08 +0300

    Fix some html issues

--- files/rootfs/var/www/homevhost/homeprojects.php
+++ files/rootfs/var/www/homevhost/homeprojects.php
@@ -1,18 +1,20 @@
-<!DOCTYPE HTML>
+<!DOCTYPE html>
 
 <html>
  <head>
+  <meta charset="UTF-8">
   <title>Homepages and Projects</title>
   <link rel="stylesheet" href="main.css">
   <link rel="icon" href="favicon.ico">
  </head>
 
- <body class="front-page">
+ <body>
+  <div class="front-page">
 
-  <hgroup>
-   <h1><object data="logo.png" height=134 width=150><img src="logo.png" alt="" height=101 width=138></object> Homepages and Projects</h1>
-   <h2>A <a href="/">home server</a> for friends and family</h2>
-  </hgroup>
+   <div class="hgroup">
+    <h1><object data="logo.png" height=134 width=150><img src="logo.png" alt="" height=101 width=138></object> Homepages and Projects</h1>
+    <h2>A <a href="/">home server</a> for friends and family</h2>
+   </div>
 
 <?php
 
@@ -46,16 +48,17 @@ foreach ($dir_it as $file) {
     $fn = $file->getFilename();
     if ($fn !== '.' && $fn !== '..') {
         if (glob("$dir/$fn/*") != false) {
-            echo "   <a class=\"ss\" href=\"/$fn\"><strong>$fn</strong></a>\n";
+            echo "    <a class=\"ss\" href=\"/$fn\"><strong>$fn</strong></a>\n";
         }
     }
 }
 ?>
 
-  <div class="footer">
-   <address>Queries should be directed to <a href="mailto:hello [at] mehho [dot] net">hello [at] mehho [dot] net</a>.</address>
-   <p class="copyright">&copy; 2011-2013 S&eacutebastien Lerique. Design inspired from the <a href="http://www.whatwg.org/">WHATWG</a> website, thank you to them :).</p>
-  </div>
+   <div class="footer">
+    <address>Queries should be directed to <a href="mailto:hello [at] mehho [dot] net">hello [at] mehho [dot] net</a>.</address>
+    <p class="copyright">&copy; 2011-2013 S&eacute;bastien Lerique. Design inspired from the <a href="http://www.whatwg.org/">WHATWG</a> website, thank you to them :).</p>
+   </div>
 
+  </div>
  </body>
 </html>
