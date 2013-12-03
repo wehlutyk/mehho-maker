29ddc9d3ea1b169e17f9f211a38be6147901ee95
Sébastien Lerique <sl@mehho.net>
2013-12-03 15:37:20 +0100

    Add pool config for php-fpm

--- php.mk
+++ php.mk
@@ -4,6 +4,8 @@ php-pkg.i :
 
 php5-fpm-config.i : php-pkg.i utils.i
 	$(jinja_copy) $(files_dir)/etc/php5/fpm/php.ini.jinja $(settings) /etc/php5/fpm/php.ini
+	mkdir -p /etc/php5/fpm/pool.d
+	cp $(files_dir)/etc/php5/fpm/pool.d/www.conf /etc/php5/fpm/pool.d/www.conf
 	touch $@
 
 php5-fpm-service.i : php-pkg.i php5-fpm-config.i setup-php5-sqlite2.i php5-sqlite2-config.i
